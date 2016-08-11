15d21e710e6ae96ddb832003581edc91d1f22446
Erick Bosco <erickbosco@hotmail.com>
2016-08-11 18:40:22 -0300

    implement DataMapper

---
+++ data-mapper/free/User.php
@@ -0,0 +1,36 @@
+<?php
+
+/* 
+ * Data Mapper pattern example
+ * 
+ * @author Erick Bosco
+ */
+
+class User {
+    
+    private $id;
+    private $username;
+    private $password;
+    
+    public function setId($id) {
+        $this->id = $id;
+    }
+    public function getId() {
+        return $this->id;
+    }
+    
+    public function setUsername($username) {
+        $this->username = $username;
+    }
+    public function getUsername() {
+        return $this->username;
+    }
+    
+    public function setPassword($password) {
+        $this->password = $password;
+    }
+    public function getPassword() {
+        return $this->password;
+    }
+
+}
---
+++ data-mapper/free/UserMapper.php
@@ -0,0 +1,34 @@
+<?php
+
+/* 
+ * Data Mapper pattern example
+ * 
+ * @author Erick Bosco
+ */
+
+class UserMapper {
+    
+    protected $db;
+    
+    public function __construct(PDO $db) {
+        $this->db = $db;
+    }
+    
+    public function save(User $user) {
+        if (empty($user->id)) {
+            $statement = $this->db->prepare('INSERT INTO user (username, password) VALUES (?, ?)');
+            $statement->bindValue(1, $user->getUsername());
+            $statement->bindValue(2, $user->getPassword());
+            $statement->execute();
+            return $this->db->lastInsertId();
+        } else {
+            $statement = $this->db->prepare('UPDATE user SET username = ?, password = ? WHERE id = ?');
+            $statement->bindValue(1, $user->getUsername());
+            $statement->bindValue(2, $user->getPassword());
+            $statement->bindValue(3, $user->getId());
+            $statement->execute();
+            return $user->getId();
+        }
+    }
+
+}
