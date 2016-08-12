eadb857fda6eee771de4a1573e4e5278ff469354
Erick Bosco <erickbosco@hotmail.com>
2016-08-11 21:06:00 -0300

    implement ActiveRecord

--- active-record/free/User.php
+++ active-record/free/User.php
@@ -41,13 +41,13 @@ class User {
     
     public function save() {
         if (empty($this->id)) {
-            $statement = $this->db->prepare('INSERT INTO user (username, password) VALUES (:username, :password)');
+            $statement = $this->db->prepare('INSERT INTO users (username, password) VALUES (:username, :password)');
             $statement->bindParam('username', $this->username);
             $statement->bindParam('password', $this->password);
             $statement->execute();
             $this->id = $this->db->lastInsertId();
         } else {
-            $statement = $this->db->prepare('UPDATE user SET username =:username, password = :password WHERE id = :id');
+            $statement = $this->db->prepare('UPDATE users SET username =:username, password = :password WHERE id = :id');
             $statement->bindParam('id', $this->id);
             $statement->bindParam('username', $this->username);
             $statement->bindParam('password', $this->password);
