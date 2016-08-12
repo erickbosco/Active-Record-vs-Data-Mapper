f2d947f3caca7ff4428010f27466f2706fd1e855
Erick Bosco <erickbosco@hotmail.com>
2016-08-11 21:05:23 -0300

    implement DataMapper

--- data-mapper/free/UserDAO.php
+++ data-mapper/free/UserDAO.php
@@ -16,13 +16,13 @@ class UserDAO {
     
     public function save(User $user) {
         if (empty($user->id)) {
-            $statement = $this->db->prepare('INSERT INTO user (username, password) VALUES (?, ?)');
+            $statement = $this->db->prepare('INSERT INTO users (username, password) VALUES (?, ?)');
             $statement->bindValue(1, $user->getUsername());
             $statement->bindValue(2, $user->getPassword());
             $statement->execute();
             return $this->db->lastInsertId();
         } else {
-            $statement = $this->db->prepare('UPDATE user SET username = ?, password = ? WHERE id = ?');
+            $statement = $this->db->prepare('UPDATE users SET username = ?, password = ? WHERE id = ?');
             $statement->bindValue(1, $user->getUsername());
             $statement->bindValue(2, $user->getPassword());
             $statement->bindValue(3, $user->getId());
